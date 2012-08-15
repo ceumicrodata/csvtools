3d41a321f2413b7224d22f268332d91193a4abf9
Krisztian Fekete <fkr972@gmail.com>
2012-08-15 16:56:50 +0200

    csvtools: extract generic process

--- csvtools/select.py
+++ csvtools/select.py
@@ -3,24 +3,27 @@ import csv
 from csvtools.field import RecordTransformer
 
 
-def process(input_file, output_file, transform_spec):
-    reader = csv.reader(input_file)
-    writer = csv.writer(output_file)
-    transformer = RecordTransformer(transform_spec)
-
-    header = reader.next()
+def process(reader, writer, transformer):
+    reader_iter = iter(reader)
+    header = reader_iter.next()
     transformer.bind(header)
     transform = transformer.transform
 
     writer.writerow(transformer.output_header)
-    for record in reader:
+    for record in reader_iter:
         writer.writerow(transform(record))
+
+
+def select(input_file, output_file, transform_spec):
+    reader = csv.reader(input_file)
+    writer = csv.writer(output_file)
+    transformer = RecordTransformer(transform_spec)
+    process(reader, writer, transformer)
 
 
 def main():
     transform_spec = sys.argv[1]
-
-    process(sys.stdin, sys.stdout, transform_spec)
+    select(sys.stdin, sys.stdout, transform_spec)
 
 
 if __name__ == '__main__':
--- csvtools/test_select.py
+++ csvtools/test_select.py
@@ -1,6 +1,7 @@
 #! -*- encoding: utf8 -*-
 import unittest
 import csvtools.select as m
+from csvtools.test import ReaderWriter
 from StringIO import StringIO
 import csv
 
@@ -8,12 +9,24 @@ import csv
 class Test_process(unittest.TestCase):
 
     def test_change_order(self):
+        reader = ReaderWriter()
+        reader.rows = [('a', 'b'), (1, 2)]
+        writer = ReaderWriter()
+
+        m.process(reader, writer, m.RecordTransformer('b,a'))
+
+        self.assertEqual([('b', 'a'), (2, 1)], writer.rows)
+
+
+class Test_select(unittest.TestCase):
+
+    def test_change_order(self):
         input_file = StringIO(
             'a,b\n'
             '1,2')
         output_file = StringIO()
 
-        m.process(input_file, output_file, 'b,a')
+        m.select(input_file, output_file, 'b,a')
 
         r = csv.reader(StringIO(output_file.getvalue()))
         self.assertEqual(('b', 'a'), tuple(r.next()))
@@ -25,7 +38,7 @@ class Test_process(unittest.TestCase):
             '1.2345678901000,2')
         output_file = StringIO()
 
-        m.process(input_file, output_file, 'a,b')
+        m.select(input_file, output_file, 'a,b')
 
         r = csv.reader(StringIO(output_file.getvalue()))
         self.assertEqual(('a', 'b'), tuple(r.next()))
@@ -36,7 +49,7 @@ class Test_process(unittest.TestCase):
             u'a,b\nárvíztűrőtükörfúrógép,2'.encode('utf8'))
         output_file = StringIO()
 
-        m.process(input_file, output_file, 'a,b')
+        m.select(input_file, output_file, 'a,b')
 
         r = csv.reader(StringIO(output_file.getvalue()))
         self.assertEqual(('a', 'b'), tuple(r.next()))
