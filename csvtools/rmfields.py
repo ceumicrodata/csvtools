36ef7f65bd3895499eb92ae50843ce034727acbb
Krisztian Fekete <fkr972@gmail.com>
2012-08-15 17:58:50 +0200

    csvtools.rmfields

---
+++ csvtools/rmfields.py
@@ -0,0 +1,42 @@
+'''
+Remove the given fields from its input
+
+Usage:
+
+rmfields.py field_name [field_name [...]]
+'''
+
+import sys
+import csv
+from csvtools.transformer import Transformer, RecordTransformer
+
+
+
+class RemoveFields(Transformer):
+
+    def __init__(self, fields_to_remove):
+        self.fields_to_remove = fields_to_remove
+        self.transformer = None
+
+    def bind(self, header):
+        self.output_header = tuple(
+            field_name
+            for field_name in header
+            if field_name not in self.fields_to_remove)
+        self.transformer = RecordTransformer(','.join(self.output_header))
+        self.transformer.bind(header)
+
+    @property
+    def transform(self):
+        return self.transformer.transform
+
+
+def main():
+    reader = csv.reader(sys.stdin)
+    writer = csv.writer(sys.stdout)
+    fields = sys.argv[1:]
+    RemoveFields(fields).process(reader, writer)
+
+
+if __name__ == '__main__':
+    main()
---
+++ csvtools/test_rmfields.py
@@ -0,0 +1,26 @@
+import unittest
+from mock import sentinel
+import csvtools.rmfields as m
+from csvtools.test import ReaderWriter
+
+
+class Test_RemoveFields(unittest.TestCase):
+
+    def test(self):
+        reader = ReaderWriter()
+        reader.rows = [
+            ('aa', 'bb', 'cc', 'dd'),
+            (sentinel.aa1, sentinel.bb1, sentinel.cc1, sentinel.dd1),
+            (sentinel.aa2, sentinel.bb2, sentinel.cc2, sentinel.dd2),
+            ]
+        writer = ReaderWriter()
+
+        m.RemoveFields(['cc', 'aa']).process(reader, writer)
+
+        self.assertEqual(
+            [
+            ('bb', 'dd'),
+            (sentinel.bb1, sentinel.dd1),
+            (sentinel.bb2, sentinel.dd2),
+            ],
+            writer.rows)
