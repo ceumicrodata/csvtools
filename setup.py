be009b2a0a58ac32bc1109a123f455309d5b63e6
Krisztian Fekete <fkr972@gmail.com>
2013-03-12 21:12:48 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf8
+
+from setuptools import setup
+
+setup(name='csvtools',
+      version='0.1.0-dev',
+      description=u'Tools for transforming .csv files',
+      author=u'Krisztián Fekete',
+#       author_email='fkr972',
+#       url='http://maybe.later',
+      packages=['csvtools'],
+      # install_requires=[],
+      provides=['csvtools (0.1.0)']
+     )
