81a0af8c5fb34ca3a7e7a1ae923473a54168f9dd
Krisztian Fekete <fkr972@gmail.com>
2013-03-13 23:45:21 +0100

    split tool (still need some work)

---
+++ csvtools/split.py
@@ -0,0 +1,46 @@
+import csv
+
+
+class StreamSplitter(object):
+
+    def __init__(self, reader, prefix, chunk_size):
+        self.prefix = prefix
+        self.chunk_size = chunk_size
+        self.file_index = 0
+        self.reader = iter(reader)
+        self.header = self.reader.next()
+        self.output_file = None
+
+    def close_file(self):
+        self.output_file.close()
+        self.output_file = None
+        self.file_index += 1
+
+    def new_file(self):
+        if self.output_file is not None:
+            self.close_file()
+
+        self.output_file = open(self.prefix + str(self.file_index), 'w')
+        writer = csv.writer(self.output_file)
+        writer.writerow(self.header)
+        return writer
+
+    def split(self):
+        chunk_size = self.chunk_size
+
+        writer = self.new_file()
+        count = 0
+
+        for row in self.reader:
+            if count == chunk_size:
+                writer = self.new_file()
+                count = 0
+
+            writer.writerow(row)
+            count += 1
+
+        self.close_file()
+
+
+def split(reader, prefix, chunk_size):
+    StreamSplitter(reader, prefix, chunk_size).split()
