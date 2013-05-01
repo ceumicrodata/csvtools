588831dbd995f46ba2558c19d26169a574b6b77d
Krisztian Fekete <fkr972@gmail.com>
2013-05-01 21:54:38 +0200

    console scripts

--- setup.py
+++ setup.py
@@ -3,12 +3,24 @@
 
 from setuptools import setup
 
-setup(name='csvtools',
-      version='0.1.0-dev',
-      description=u'Tools for transforming .csv files',
-      author=u'CEU MicroData',
-      url='https://github.com/ceumicrodata/csvtools',
-      packages=['csvtools'],
-      install_requires=['temp_dir'],
-      provides=['csvtools (0.1.0)']
-     )
+setup(
+    name='csvtools',
+    version='0.1.0-dev',
+    description=u'Tools for transforming .csv files',
+    author=u'CEU MicroData',
+    url='https://github.com/ceumicrodata/csvtools',
+    packages=['csvtools'],
+    install_requires=['temp_dir'],
+    provides=['csvtools (0.1.0)'],
+    entry_points={
+        'console_scripts': [
+            'csv_select = csvtools.select:main',
+            'csv_split = csvtools.split:main',
+            'csv_zip = csvtools.zip:main',
+            'csv_unzip = csvtools.unzip:main',
+            'csv_rmfields = csvtools.rmfields:main',
+            'csv_extract_map = csvtools.extract_map:main',
+            'csv_to_postgres = csvtools.to_postgres:main',
+        ],
+    }
+    )
